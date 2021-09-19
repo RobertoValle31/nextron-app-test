efaf8b67a55c8c872cc727731254233cdf56896e
Roberto <roberto@opticpower.com>
2021-09-19 16:41:09 -0400

    feat(AutoUpdater)-Implementing-auto-updater

--- main/background.ts
+++ main/background.ts
@@ -1,32 +1,40 @@
-import { app } from 'electron';
-import serve from 'electron-serve';
-import { createWindow } from './helpers';
+import { app } from "electron";
+import serve from "electron-serve";
+import { createWindow } from "./helpers";
 
-const isProd: boolean = process.env.NODE_ENV === 'production';
+import { autoUpdater } from "electron-updater";
+
+const isProd: boolean = process.env.NODE_ENV === "production";
 
 if (isProd) {
-  serve({ directory: 'app' });
+  serve({ directory: "app" });
 } else {
-  app.setPath('userData', `${app.getPath('userData')} (development)`);
+  app.setPath("userData", `${app.getPath("userData")} (development)`);
 }
 
 (async () => {
   await app.whenReady();
 
-  const mainWindow = createWindow('main', {
+  const mainWindow = createWindow("main", {
     width: 1000,
     height: 600,
   });
 
   if (isProd) {
-    await mainWindow.loadURL('app://./home.html');
+    await mainWindow.loadURL("app://./home.html");
   } else {
     const port = process.argv[2];
     await mainWindow.loadURL(`http://localhost:${port}/home`);
     mainWindow.webContents.openDevTools();
   }
+
+  // This will immediately download an update, then install when the app quits.
+  app.on("ready", function () {
+    console.log("updating....");
+    autoUpdater.checkForUpdatesAndNotify();
+  });
 })();
 
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   app.quit();
 });
