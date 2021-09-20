e2128e4ae3805648ad53175da3cfb6c77eb6a325
Roberto <roberto@opticpower.com>
2021-09-20 09:19:26 -0400

    Adding new version

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -46,7 +46,7 @@ function Home() {
           Connect Marketing
         </Typography>
         <Typography variant="subtitle2" gutterBottom>
-          Version: 1.0.21
+          Version: 1.0.22
         </Typography>
         <img src="/images/logo.png" />
         <Typography gutterBottom>
